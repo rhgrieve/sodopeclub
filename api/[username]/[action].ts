08e82f194a6519aba7da10240b91badffcd85960
Harrison Grieve <rhg@alkmi.io>
2022-08-06 21:27:30 +0200

    refactor: clean up error handling and move to readFileSync

--- api/[username]/[action].ts
+++ api/[username]/[action].ts
@@ -1,5 +1,5 @@
 import fs from "fs";
-import path from 'path';
+import path from "path";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 type SitesData = {
@@ -13,58 +13,50 @@ enum Action {
   Random = "random",
 }
 
-function handleError(message: string, response: VercelResponse) {
-  console.error(message);
+function handleError(error: Error, response: VercelResponse) {
+  console.error(error.message);
   response.redirect("/");
 }
 
 export default (request: VercelRequest, response: VercelResponse) => {
   const { username, action } = request.query;
 
-  const allowedActions = Object.values(Action);
-  if (!allowedActions.includes(<Action>action)) {
-    handleError(`invalid action [${action}]`, response);
-  }
-
-  fs.readFile(path.join(process.cwd(), 'src', '_data', 'sites.json'), (err, data) => {
-    if (err) {
-        handleError(err.message, response)
-        return;
+  try {
+    const allowedActions = Object.values(Action);
+    if (!allowedActions.includes(<Action>action)) {
+      throw new Error(`invalid action [${action}]`);
     }
 
-    try {
-      const parsed: SitesData = JSON.parse(data.toString());
-      let idx = parsed.map((el) => el.username).indexOf(<string>username);
-      if (idx < 0) {
-        handleError(`username [${username}] not found`, response);
-        return;
-      }
-
-      switch (action) {
-        case Action.Next:
-          idx = idx >= parsed.length - 1 ? 0 : idx + 1;
-          break;
-        case Action.Prev:
-          idx = idx <= 0 ? parsed.length - 1 : idx - 1;
-          break;
-        case Action.Random:
-          idx = Math.floor(Math.random() * parsed.length);
-          break;
-        default:
-      }
-
-      const redirectUrl = parsed[idx].url;
-      if (!redirectUrl) {
-        handleError(`no redirect URL found for username [${username}]`, response);
-        return;
-      }
-
-      response.redirect(redirectUrl)
-
-    } catch (e) {
-      handleError(e.message, response);
+    const dataPath = path.join(process.cwd(), "src", "_data", "sites.json");
+    const data = fs.readFileSync(dataPath);
+    const parsed: SitesData = JSON.parse(data.toString());
+    let idx = parsed.map((user) => user.username).indexOf(<string>username);
+    if (idx < 0) {
+      throw new Error(`username [${username}] not found`);
     }
+
+    switch (action) {
+      case Action.Next:
+        idx = idx >= parsed.length - 1 ? 0 : idx + 1;
+        break;
+      case Action.Prev:
+        idx = idx <= 0 ? parsed.length - 1 : idx - 1;
+        break;
+      case Action.Random:
+        idx = Math.floor(Math.random() * parsed.length);
+        break;
+      default:
+    }
+
+    const redirectUrl = parsed[idx].url;
+    if (!redirectUrl) {
+      throw new Error(`no redirect URL found for username [${username}]`);
+    }
+
+    response.redirect(redirectUrl);
 
     response.end();
-  });
+  } catch (e) {
+    handleError(e, response);
+  }
 };
